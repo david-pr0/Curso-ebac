cbf1adea54153d696f4e82b958d19296e98d5137
David <rafael.santos.jb22@gmail.com>
2024-04-11 16:33:47 -0300

    commit inicial

--- js/script.js
+++ js/script.js
@@ -1,19 +1,50 @@
-const form = document.querySelector('.formulario')
-let numero1 = document.querySelector('#numero1');
-let numero2 = document.querySelector('#numero2');
-let valido = document.querySelector('.valido');
-let invalido = document.querySelector('.invalido');
+const form = document.querySelector('.form');
+const campo = document.querySelector('tbody');
+let nome = document.querySelector('#nome');
+let telefone = document.querySelector('#telefone');
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
+    criarLinha();
+});
 
-    validar(numero1.value, numero2.value)
-})
+function criarLinha() {
+    let linha = document.createElement('tr');
+    // Campo para nomes
+    let novaLinha = document.createElement('td');
+    let conteudo = document.createTextNode(nome.value);
+    // Campo para telefones
+    let novoTelefone = document.createElement('td');
+    let conteudoTelefone = document.createTextNode(telefone.value);
+    // Atribuindo valores
+    novaLinha.appendChild(conteudo);
+    novoTelefone.appendChild(conteudoTelefone);
+    linha.appendChild(novaLinha);
+    linha.appendChild(novoTelefone);
+    campo.appendChild(linha);
+}
 
-function validar (n1, n2) {
-    if (n2 > n1) {
-        valido.style.display = 'block'
+telefone.addEventListener('input', function(e) {
+    formatarTelefone(telefone.value);
+});
+
+function formatarTelefone(input) {
+    // Remove todos os caracteres não numéricos
+    const numero = input.replace(/\D/g, '');
+
+    // Formata o número de telefone
+    if (numero.length === 10) {
+        const parte1 = numero.slice(0, 2);
+        const parte2 = numero.slice(2, 6);
+        const parte3 = numero.slice(6, 10);
+        telefone.value = `(${parte1}) ${parte2}-${parte3}`;
+    } else if (numero.length === 11) {
+        const parte1 = numero.slice(0, 2);
+        const parte2 = numero.slice(2, 7);
+        const parte3 = numero.slice(7, 11);
+        telefone.value = `(${parte1}) ${parte2}-${parte3}`;
     } else {
-        invalido.style.display = 'block'
+        // Se o número não tiver 10 ou 11 dígitos, exibe o número sem formatação
+        telefone.value = numero;
     }
 }
