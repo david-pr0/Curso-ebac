1b480f41c82574ac154c6aa9dde4fbf102eb5fe7
David <rafael.santos.jb22@gmail.com>
2024-07-06 18:41:46 -0300

    commit inicial

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
+function Pessoa(nome, idade, sexo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.sexo = sexo;
+}
 
+function Funcionario(nome, idade, sexo, cargo) {
+    let _cargo = cargo;
+    Pessoa.call(this, nome, idade, sexo);
+    this.getCargo = function() {
+        return _cargo;
+    }
+}
 
-    const endPoint = 'https://api.github.com/users/david-pr0';
-    fetch(endPoint).then(function(resposta) {
-        return resposta.json();
-    })
-    .then(function(json) {
-        const profileName = json.name;
-        const profileUserName = json.login;
-        const avatar = json.avatar_url;
-        const repositories = json.public_repos;
-        const followers = json.followers;
-        const following = json.following;
-        const url = json.html_url;
+function Aluno(nome, idade, sexo, curso) {
+    let _curso = curso;
+    Pessoa.call(this, nome, idade, sexo);
+    this.getCurso = function() {
+        return _curso;
+    }
+}
 
-        document.getElementById('profile-name').innerHTML = profileName;
-        document.getElementById('profile-username').innerHTML = profileUserName;
-        document.getElementById('profile-avatar').setAttribute('src', avatar);
-        document.getElementById('repositories').innerHTML = repositories;
-        document.getElementById('followers').innerHTML = followers;
-        document.getElementById('following').innerHTML = following;
-        document.getElementById('profile-link').setAttribute('href', url);
-    })
-})
+const funcionario1 = new Funcionario('David', 21, 'Masculino', 'dev-frontend');
+const funcionario2 = new Funcionario('Maria', 23, 'Feminino', 'dev-backend');
+const aluno1 = new Aluno('David', 21, 'Masculino', 'dev-frontend');
+const aluno2 = new Aluno('Maria', 23, 'Feminino', 'dev-backend');
+
+console.log(funcionario1, funcionario1.getCargo());
+console.log(funcionario2, funcionario2.getCargo());
+console.log(aluno1, aluno1.getCurso());
+console.log(aluno2, aluno2.getCurso());
