9e6ff2988823e4d2c531747c35f48aa9c84684c1
David <rafael.santos.jb22@gmail.com>
2024-07-10 10:16:57 -0300

    commit inicial

---
+++ main.ts
@@ -0,0 +1,10 @@
+function multiplicar(num1:number, num2: number):number {
+    return num1 * num2;
+}
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+console.log(multiplicar(7, 8));
+console.log(saudacao('David'));
