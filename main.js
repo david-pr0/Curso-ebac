2bb15531ba35038bec790ff5792e915db93ca7c4
David <rafael.santos.jb22@gmail.com>
2024-07-13 09:55:59 -0300

    commit inicial

--- main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function multiplicar(num1, num2) {
-    return num1 * num2;
-}
-function saudacao(nome) {
-    return `Olá ${nome}`;
-}
-console.log(multiplicar(7, 8));
-console.log(saudacao('David'));
